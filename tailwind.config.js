54370bfb72e8fe1ba2028b44b0571049a6dd1c53
Jade Nash <jade@lilydev.com>
2024-01-01 00:48:53 -0600

    Clean up tailwind themes & add divider color class

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,15 @@ export default {
 			light: {
 				base: '#ffffff',
 				raised: '#e5e7eb',
+
 				tooltip: '#d1d5db',
+
+				divider: '#6b7280',
+
 				'btn-hover': '#d1d5db',
 				'group-emphasized': '#d1d5db',
 				'group-emphasized-hover': '#9ca3af',
+
 				input: '#e5e7eb',
 
 				'text-text': 'black'
@@ -24,11 +29,15 @@ export default {
 			dark: {
 				base: '#1f2937',
 				raised: '#111827',
+
 				tooltip: '#374151',
+
+				divider: '#9ca3af',
+
 				'btn-hover': '#374151',
-				'btn-sidebar-hover': '#0f172a',
 				'group-emphasized': '#1e293b',
 				'group-emphasized-hover': '#0f172a',
+
 				input: '#4b5563',
 
 				'darker-text': '#e1e7f5',
@@ -39,11 +48,15 @@ export default {
 			oled: {
 				base: 'black',
 				raised: '#18181b',
+
 				tooltip: '#404040',
+
+				divider: '#a1a1aa',
+
 				'btn-hover': '#404040',
-				'btn-sidebar-hover': '#0f172a',
 				'group-emphasized': '#171717',
 				'group-emphasized-hover': '#262626',
+
 				input: '#3f3f46',
 
 				'darker-text': '#e1e7f5',
