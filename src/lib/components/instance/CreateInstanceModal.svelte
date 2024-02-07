29682e0f2f441f6f6d5d67516cd0f72bcee8ea1d
Jade Nash <jade@lilydev.com>
2024-02-07 10:48:46 -0600

    Translate create instance modal & toasts

--- src/lib/components/instance/CreateInstanceModal.svelte
+++ src/lib/components/instance/CreateInstanceModal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
+
 	import type { SvelteComponent } from 'svelte';
 	import { getModalStore, getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
 	import { invoke } from '@tauri-apps/api';
@@ -13,13 +15,13 @@
 	let formValid: boolean = true;
 
 	const createdInstanceToast: ToastSettings = {
-		message: 'Successfully created new instance!',
+		message: $_('modal.create_instance.toast.success'),
 		hoverable: true,
 		background: 'variant-soft-success'
 	};
 
 	const nameErrorToast: ToastSettings = {
-		message: 'Invalid instance name!',
+		message: $_('modal.create_instance.toast.error'),
 		hoverable: true,
 		background: 'variant-soft-error'
 	};
@@ -42,26 +44,26 @@
 
 {#if $modalStore[0]}
 	<div class="card p-4 w-modal shadow-xl space-y-4">
-		<header class="text-2xl font-bold">Create Instance</header>
-		<article>Create a new instance with a specified name.</article>
+		<header class="text-2xl font-bold">{$_('modal.create_instance.title')}</header>
+		<article>{$_('modal.create_instance.description')}</article>
 		<form class="p-2 space-y-4">
 			<label class="label" for="instanceNameInput">
-				<span>Name</span>
+				<span>{$_('modal.create_instance.form.name.label')}</span>
 				<input
 					class="input {formValid ? '' : 'input-error'} py-1.5 px-2"
 					type="text"
 					bind:value={instanceName}
 					on:change={() => (formValid ? {} : (formValid = true))}
-					placeholder="New Instance"
+					placeholder={$_('modal.create_instance.form.name.placeholder')}
 				/>
 			</label>
 		</form>
 		<footer class={parent.regionFooter}>
 			<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}
-				>{parent.buttonTextCancel}</button
+				>{$_('modal.create_instance.action_row.cancel')}</button
 			>
 			<button class="btn {parent.buttonPositive}" on:click={onFormSubmit}
-				>Create Instance</button
+				>{$_('modal.create_instance.action_row.confirm')}</button
 			>
 		</footer>
 	</div>
